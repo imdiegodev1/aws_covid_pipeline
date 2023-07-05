4c6c6a4a35a4320b5bcf4c3d4f2dd59ee944c67c
imdiegodev1 <diegoanr40@hotmail.com>
2023-07-04 21:18:33 -0500

    crete bucket and objects

--- 1_bucket_load_data.py
+++ 1_bucket_load_data.py
@@ -1,27 +1,33 @@
 import boto3
 
-BUCKET_NAME = ""
+BUCKET_NAME = "test-bucket-diego9475"
+DIRECTORIES = ["enigma-jhud", "enigma-nytimes-data-in-usa/us_county",
+               "enigma-nytimes-data-in-usa/us_state/"]
 
 def main():
 
     s3_connection = connection()
-    create_bucket(BUCKET_NAME, s3_connection)
+    #create_bucket(s3_connection, BUCKET_NAME)
+    create_folders(s3_connection, BUCKET_NAME, DIRECTORIES)
 
 #Connection
 def connection():
 
-    s3 = boto3.resource('s3')
+    s3 = boto3.client('s3')
 
     return s3
 
 #Create bucket
-def create_bucket(BUCKET_NAME: str, s3_connection):
+def create_bucket(s3_connection, BUCKET_NAME: str):
     
     s3_connection.create_bucket(Bucket= BUCKET_NAME)
 
 #Create Folders
-def create_folders():
+def create_folders(s3_connection, BUCKET_NAME: str, DIRECTORIES: list):
     
+    for i in DIRECTORIES:
+        s3_connection.put_object(Bucket = BUCKET_NAME,
+                                 Key = (i + '/'))
 
 if __name__ == '__main__':
 
